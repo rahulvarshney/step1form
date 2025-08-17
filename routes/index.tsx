caae70846fc9d987558221dbac6070e60fabddaf
rahulvarshney <rahulvarshney@gmail.com>
2025-08-17 07:38:20 -0700

    0.0.3

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,37 @@
-import { useSignal } from "@preact/signals";
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    return await ctx.render();
+  },
+  async POST(req, ctx) {
+    const form = await req.formData();
+    const email = form.get("email")?.toString();
+
+    // Add email to list.
+
+    // Redirect user to thank you page.
+    const headers = new Headers();
+    headers.set("location", "/thanks-for-subscribing");
+    return new Response(null, {
+      status: 303, // See Other
+      headers,
+    });
+  },
+};
+
+export default function Subscribe() {
+  return (
+    <>
+      <form action="https://step2x-sj61ntjs0pym.rahulvarshney.deno.net">
+        <input name="from" value="support@740bsecure.com" />
+        <button type="submit">Search</button>
+      </form>
+    </>
+  );
+}
+
+/*import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
@@ -23,3 +56,4 @@ export default function Home() {
     </div>
   );
 }
+*/
